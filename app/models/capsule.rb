f1a505dbb231be368b45f3350d089f953949fba0
emily <emilyvicki601@gmail.com>
2022-05-27 02:47:15 +0800

    refactor

--- app/models/capsule.rb
+++ app/models/capsule.rb
@@ -1,14 +1,15 @@
 # frozen_string_literal: true
 
 module TimeCapsule
-    # Behaviors of the currently logged in account
-    class Capsule
-      attr_reader :id, :name, :type
-  
-      def initialize(cap_info)
-        @id = cap_info['data']['attributes']['id']
-        @name = cap_info['data']['attributes']['name']
-        @type = cap_info['data']['attributes']['type']
-      end
+  # Behaviors of the currently logged in account
+  class Capsule
+    attr_reader :id, :name, :type, # basic info
+                :letters, :policies # full details
+
+    def initialize(cap_info)
+      @id = cap_info['data']['attributes']['id']
+      @name = cap_info['data']['attributes']['name']
+      @type = cap_info['data']['attributes']['type']
     end
   end
+end
