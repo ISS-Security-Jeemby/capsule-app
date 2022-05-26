3e07dccc8d3ddc4818545663ea7d50d25e1101fc
emily <emilyvicki601@gmail.com>
2022-05-27 02:46:00 +0800

    feat: add services

---
+++ app/services/get_capsule.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'http'
+
+module TimeCapsule
+  # Return a capsule of an account
+  class GetCapsule
+    def initialize(config)
+      @config = config
+    end
+
+    def call(current_account, capsule_id)
+      response = HTTP.auth("Bearer #{current_account.auth_token}")
+                     .get("#{@config.API_URL}/capsules/#{capsule_id}")
+
+      response.code == 200 ? JSON.parse(response.body.to_s)['data'] : nil
+    end
+  end
+end
