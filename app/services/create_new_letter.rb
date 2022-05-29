64fe4b5b7034a3081ab97e70b7115972144f5464
emily <emilyvicki601@gmail.com>
2022-05-30 00:49:35 +0800

    feat: add modals for new letter and collaborator

--- app/services/create_new_letter.rb
+++ app/services/create_new_letter.rb
@@ -16,8 +16,7 @@ module TimeCapsule
     def call(current_account:, capsule_id:, letter_data:)
       config_url = "#{api_url}/capsules/#{capsule_id}/letters"
       response = HTTP.auth("Bearer #{current_account.auth_token}")
-                     .post(config_url, json: letter_data)
-
+                     .post(config_url, json: letter_data.to_json)
       response.code == 201 ? JSON.parse(response.body.to_s) : raise
     end
   end
