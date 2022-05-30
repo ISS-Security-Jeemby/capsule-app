170828c663193c50779fbe94d3d1fdafcb37f6bc
emily <emilyvicki601@gmail.com>
2022-05-30 10:40:24 +0800

    refactor: letter's data format

--- app/services/create_new_letter.rb
+++ app/services/create_new_letter.rb
@@ -16,7 +16,7 @@ module TimeCapsule
     def call(current_account:, capsule_id:, letter_data:)
       config_url = "#{api_url}/capsules/#{capsule_id}/letters"
       response = HTTP.auth("Bearer #{current_account.auth_token}")
-                     .post(config_url, json: letter_data.to_json)
+                     .post(config_url, json: letter_data)
       response.code == 201 ? JSON.parse(response.body.to_s) : raise
     end
   end
