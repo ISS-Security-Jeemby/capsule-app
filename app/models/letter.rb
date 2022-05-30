68014fa2bc4660b1b2f101bdacb2f8583680a4c2
emily <emilyvicki601@gmail.com>
2022-05-31 03:02:12 +0800

    refactor: add is_locked in letter

--- app/models/letter.rb
+++ app/models/letter.rb
@@ -5,13 +5,11 @@ require_relative 'capsule'
 module TimeCapsule
   # Behaviors of the currently logged in account
   class Letter
-    attr_reader :id, :title, :receiver_id, :status, :is_private, # basic info
-                :content,
-                :capsule # full details
+    attr_reader :id, :title, :receiver_id, :status, :is_locked, :is_private, # basic info
+                :content
 
     def initialize(info)
       process_attributes(info['attributes'])
-      process_included(info['include'])
     end
 
     private
@@ -21,12 +19,9 @@ module TimeCapsule
       @title          = attributes['title']
       @receiver_id    = attributes['receiver_id']
       @status         = attributes['status']
+      @is_locked      = attributes['is_locked']
       @is_private     = attributes['is_private']
       @content        = attributes['content']
-    end
-
-    def process_included(included)
-      @capsule = Capsule.new(included['capsule'])
     end
   end
 end
