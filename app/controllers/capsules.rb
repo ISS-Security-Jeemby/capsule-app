9186a05f8c9b353ae612cbec319a9808953eb031
emily <emilyvicki601@gmail.com>
2022-05-31 10:00:14 +0800

    fix: create letter routing

--- app/controllers/capsules.rb
+++ app/controllers/capsules.rb
@@ -9,7 +9,6 @@ module TimeCapsule
       routing.on do
         routing.redirect '/auth/login' unless @current_account.logged_in?
         @capsules_route = '/capsules'
-
         routing.on(String) do |capsule_id|
           @capsule_route = "#{@capsules_route}/#{capsule_id}"
           routing.on 'letters' do
@@ -34,6 +33,27 @@ module TimeCapsule
                 }
               end
             end
+
+            # POST /capsules/[capsule_id]/letters/
+            routing.post do
+              letter_data = Form::NewLetter.new.call(routing.params)
+              if letter_data.failure?
+                flash[:error] = Form.message_values(letter_data)
+                routing.halt
+              end
+
+              CreateNewLetter.new(App.config).call(
+                current_account: @current_account,
+                capsule_id:,
+                letter_data: letter_data.to_h
+              )
+              flash[:notice] = 'Your letter was added'
+            rescue StandardError => e
+              puts e.full_message
+              flash[:error] = 'Could not add letter'
+            ensure
+              routing.redirect @capsule_route
+            end
           end
 
           # GET /capsules/[capsule_id]
@@ -52,28 +72,6 @@ module TimeCapsule
             puts e.full_message
             flash[:error] = 'Capsule not found'
             routing.redirect @capsules_route
-          end
-
-          # POST /capsules/[capsule_id]/letters/
-          routing.post('letters') do
-            letter_data = Form::NewLetter.new.call(routing.params)
-            if letter_data.failure?
-              flash[:error] = Form.message_values(letter_data)
-              routing.halt
-            end
-
-            CreateNewLetter.new(App.config).call(
-              current_account: @current_account,
-              capsule_id:,
-              letter_data: letter_data.to_h
-            )
-            flash[:notice] = 'Your letter was added'
-          rescue StandardError => e
-            puts e.inspect
-            puts e.backtrace
-            flash[:error] = 'Could not add letter'
-          ensure
-            routing.redirect @capsule_route
           end
         end
 
