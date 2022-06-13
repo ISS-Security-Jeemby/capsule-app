be3c888cd258ef021232e12248ff0a3a041736a1
ruby2800 <36958049+ruby2800@users.noreply.github.com>
2022-06-13 13:51:02 +0800

    feat:add sent button

--- app/controllers/capsules.rb
+++ app/controllers/capsules.rb
@@ -28,6 +28,37 @@ module TimeCapsule
 
             routing.on String do |letter_id|
 
+              # POST capsules/[capsule_id]/letters/[letter_id]/send
+              routing.on 'send' do
+                routing.post do
+
+                  # letter =SendLetter.new(App.config)
+                  #     .call(@current_account, letter_id)
+                  #                     letter_id = routing.params['id']
+                  routing.params.delete('id')
+                  UpdateLetter.new(App.config)
+                              .call(@current_account, letter_id, routing.params, 2)
+                  routing.redirect @capsule_route
+                  # collaborators = GetLetterCollaborators.new(App.config).call(
+                  #   @current_account, letter_id:
+                  # )
+                  # capsule = GetCapsule.new(App.config)
+                  #     .call(@current_account, capsule_id)
+
+                  # change the letter status when receiver open first time
+                  # if letter['status'] == 2
+                  #   UpdateLetter.new(App.config)
+                  #       .call(@current_account, letter['id'], {}, 3)
+                  # end
+                  # is_view =  'view'
+
+                  # view :letter, locals: {
+                  #   current_account: @current_account, letter:, capsule: capsule['attributes'], collaborators:, is_view:
+                  # }
+                  # routing.redirect @capsule_route
+                end
+              end
+
               # GET capsules/[capsule_id]/letters/[letter_id]/view
               routing.on 'view' do
                 routing.get do
@@ -53,6 +84,7 @@ module TimeCapsule
                 end
               end
 
+
               # GET capsules/[capsule_id]/letters/[letter_id]/edit
               routing.on 'edit' do
                 
@@ -77,9 +109,9 @@ module TimeCapsule
                 routing.get do
                   letter = DeleteLetter.new(App.config)
                       .call(@current_account, letter_id)
-                  view :letter, locals: {
-                     current_account: @current_account, letter:
-                  }
+                  # view :letter, locals: {
+                  #    current_account: @current_account, letter:
+                  # }
                   routing.redirect @capsule_route
                 end
               end
@@ -127,6 +159,7 @@ module TimeCapsule
             )
             capsule = Capsule.new(capsule_info)
 
+            
             # get letters
             letters = if capsule.type == 3
                         # get received letters
@@ -138,6 +171,7 @@ module TimeCapsule
                           @current_account, capsule_id
                         )
                       end
+            binding.pry
             status_code = { 1 => 'Draft', 2 => 'Sended', 3 => 'Reciever Recieved' }
             letters.each do |letter|
               letter['attributes']['status'] = status_code[letter['attributes']['status']]
