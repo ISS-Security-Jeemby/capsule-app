d54f30b20c7cdbf9f109749184dc9ca31e0fa2fa
emily <emilyvicki601@gmail.com>
2022-06-16 02:20:02 +0800

    style: rubocop

--- app/services/notice_collaborator.rb
+++ app/services/notice_collaborator.rb
@@ -13,14 +13,14 @@ module TimeCapsule
       @config = config
     end
 
-    def call(current_account,collaborator_data)
+    def call(current_account, collaborator_data)
       # collaborator_token = SecureMessage.encrypt(collaborator_data)
-      
+
       response = HTTP.auth("Bearer #{current_account.auth_token}")
                      .post("#{@config.API_URL}/letters/notice", json: collaborator_data)
-      
+
       raise(ReuseEmailOrUsernameError) if response.code == 400
-      raise(VerificationError) unless response.code == 202  
+      raise(VerificationError) unless response.code == 202
 
       JSON.parse(response.to_s)
     rescue HTTP::ConnectionError
